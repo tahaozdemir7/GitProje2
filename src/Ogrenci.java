dc6648984bb1fd6378473b678ff5b77d3cede932
Lenovo <tahaozdemir7@gmail.com>
2022-07-06 13:53:26 +0300

    Ogrenci Classi ikinci bolum yapildi

--- src/Ogrenci.java
+++ src/Ogrenci.java
@@ -1,7 +1,9 @@
 public class Ogrenci {
     public static void main(String[] args) {
 
-        System.out.println("Merhaba dunya 1");
+        System.out.println("Merhaba dunya 2");
+        System.out.println("Merhaba dunya 3");
+        System.out.println("HELIN");
 
     }
 }
