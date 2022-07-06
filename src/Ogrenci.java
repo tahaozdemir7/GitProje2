532ab375977d40915bf41addeb286e266018ae77
Lenovo <tahaozdemir7@gmail.com>
2022-07-06 14:09:29 +0300

    Ogrenci Classi ucuncu bolum yapildi

--- src/Ogrenci.java
+++ src/Ogrenci.java
@@ -1,9 +1,7 @@
 public class Ogrenci {
     public static void main(String[] args) {
 
-        System.out.println("Merhaba dunya 2");
-        System.out.println("Merhaba dunya 3");
-        System.out.println("HELIN");
+        System.out.println("Norman dunyanin en iyi voleybol kulubu");
 
     }
 }
